8d3daeb1399567e74fd33ae9913919ee90990835
chrisakhula <ouma.cleo@gmail.com>
2024-02-27 21:52:39 +0300

    data types

--- main.dart
+++ main.dart
@@ -17,8 +17,39 @@ void main() {
 
     // you can also declare a variable without assingning value to it
     var x;
-    x = 20
+    x = 20;
 
     print(x);
+
+    // declaring variablles 
+    int num1 = 100;
+    double num2 = 130.2;
+    num num3 = 50;
+    num num4 = 50.4;
+
+    // for sum
+    num sum = num1 + num2 + num3 + num4;
+
+    //printing info
+    print("Num 1 is $num1");
+    print("Num 2 is $num2");
+    print("Num 3 is $num3");
+    print("Num 4 is $num4");
+    print("Sum is $sum");
+
+    //declaring values
+    String schoolName = "Powerlearn Project Academy";
+
+    // printing values
+    print("My school name is $schoolName and the address is $address");
+
+    List<String> fam = [ "Beatrice", "Judith", "Ken", "Cleophas", "Prisca", "Regina"];
+    print("Value of names is $fam");
+    print("Value of fam[0] is ${fam[0]}");
+    print("Value of fam[3] is ${fam[3]}");
+    print("Value of fam[5] is ${fam[5]}");
+
+    print(fam);
+
 
 }
