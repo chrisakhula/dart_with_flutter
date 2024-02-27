4fbdf1fed46e27c1c14e535f21637a7223fe6377
chrisakhula <ouma.cleo@gmail.com>
2024-02-27 20:58:21 +0300

    Variables on dart

--- main.dart
+++ main.dart
@@ -1,3 +1,18 @@
 void main() {
     print("Hello World!");
+
+    // declaring variables
+    String name = "Cleophas";
+    String address = "Busia KE";
+    num age = 36;
+    num height = 5.9;
+    bool isMarried = false;
+
+    // printing variables value
+    print("Name is $name");
+    print("Address is $address");
+    print("Age is $age");
+    print("Height is $height");
+    print("Married status is $isMarried");
+
 }
